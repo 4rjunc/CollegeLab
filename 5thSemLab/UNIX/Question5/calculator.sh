3e0e1a850f8e66d3f5904626f64405e8ff18ec4b
H4K3R13 <mit2224366@gmail.com>
2023-07-07 15:01:52 +0530

    Cmplyt Q5

--- 5thSemLab/UNIX/Question5/calculator.sh
+++ 5thSemLab/UNIX/Question5/calculator.sh
@@ -4,3 +4,21 @@ echo "1. Addition"
 echo "2. Substration"
 echo "3. Multiplication"
 echo "4. Division"
+
+echo " Enter Choice: "
+read choice
+echo " Enter NUM1: "
+read num1
+echo " Enter NUM2: "
+read num2
+
+case $choice in 
+1) ((result = $num1 + $num2))
+    echo "$num1 + $num2 = $result";;
+2) ((result = $num1 - $num2))
+    echo "$num1 - $num2 = $result";;
+3) ((result = $num1 * $num2))
+    echo "$num1 * $num2 = $result";;
+4) ((result = $num1 / $num2))
+    echo "$num1 / $num2 = $result";;
+esac
