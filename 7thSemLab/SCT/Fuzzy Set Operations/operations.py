d49e5292f754af0c1e9c0f9e5b5096d3b631d534
H4K3R13 <mit2224366@gmail.com>
2023-09-26 12:32:04 +0530

    Fuzzy set operations

---
+++ 7thSemLab/SCT/Fuzzy Set Operations/operations.py
@@ -0,0 +1,22 @@
+A = {
+    "a":0.2,
+    "b":0.5,
+    "c":0.8
+}
+
+B = {
+    "a":0.7,
+    "b":1.0,
+    "c":0.3
+}
+
+Z = {} # A union B
+
+for (key1,value1), (key2, value2) in zip(A.items(),B.items()):
+    # print(f"{key1=}, {value1=}, {key2=}, {value2=}")
+    if value1> value2:
+        Z[key1] = value1
+    elif value2 > value1:
+        Z[key1] = value2
+        
+print(f"{A} \n UNION \n{B}\n=\n{Z}")
