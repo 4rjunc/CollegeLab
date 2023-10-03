cef51a0cf2bd8e3fe8b186d2dae9ee6ed8f7190e
Cody McCodePants <cody@mccodepants.com>
2023-10-03 12:19:16 +0530

    Set Operations

--- 7thSemLab/SCT/Fuzzy Set Operations/operations.py
+++ 7thSemLab/SCT/Fuzzy Set Operations/operations.py
@@ -1,24 +1,37 @@
-from pprint import pprint
+# from pprint import pprint
 
-A = {
-    "a":0.2,
-    "b":0.5,
-    "c":0.8
-}
+# for (key1,value1), (key2, value2) in zip(A.items(),B.items()):
+#     # print(f"{key1=}, {value1=}, {key2=}, {value2=}")
+#     if value1> value2:
+#         Z[key1] = value1
+#     elif value2 > value1:
+#         Z[key1] = value2
 
-B = {
-    "a":0.7,
-    "b":1.0,
-    "c":0.3
-}
+Z = {}
 
-Z = {} # A union B
 
-for (key1,value1), (key2, value2) in zip(A.items(),B.items()):
-    # print(f"{key1=}, {value1=}, {key2=}, {value2=}")
-    if value1> value2:
-        Z[key1] = value1
-    elif value2 > value1:
-        Z[key1] = value2
-        
-pprint(f"{A}  UNION {B} = {Z}")
+def union(A, B):
+    for x in A and B:
+        Z[x] = max(A[x], B[x])
+    return Z
+
+
+def intersection(A, B):
+    for x in A and B:
+        Z[x] = min(A[x], B[x])
+    return Z
+
+
+def compliment(X):
+    for x in X:
+        Z[x] = round(1 - X[x],2)
+    return Z
+
+
+A = {"a": 0.2, "b": 0.5, "c": 0.8}
+B = {"a": 0.7, "b": 1.0, "c": 0.3}
+
+print(f"{A=} UNION {B=} Is {union(A,B)}")
+print(f"{A=} Intersection {B=} Is {intersection(A,B)}")
+print(f"COMPLIMENT OF {A=} IS {compliment(A)}")
+print(f"COMPLIMENT OF {B=} IS {compliment(B)}")
