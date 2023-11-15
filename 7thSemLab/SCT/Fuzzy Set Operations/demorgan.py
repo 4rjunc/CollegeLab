c48a2e1f1926c6a50a7210e0fb5a29d548f029d8
Cody McCodePants <cody@mccodepants.com>
2023-11-15 12:09:24 +0530

    8, 9. image processing

--- 7thSemLab/SCT/Fuzzy Set Operations/demorgan.py
+++ 7thSemLab/SCT/Fuzzy Set Operations/demorgan.py
@@ -10,7 +10,12 @@ def compliment(X):
         Z[x] = round(1 - X[x],2)
     return Z
 
-input("Program to prove DeMorgans Theorem!")
+def intersection(A, B):
+    for x in A and B:
+        Z[x] = min(A[x], B[x])
+    return Z
+
+print("Program to prove DeMorgans Theorem!")
 
 A = {}
 B = {}
@@ -18,14 +23,19 @@ B = {}
 no_items = int(input("Enter the numbers: "))
 for _ in range(no_items):
     key = input("Enter the crispy set elements: ")
-    value = float(input("Enter the Membership value for Set 1: "))
+    value = float(input(f"Enter the Membership value for {key} in Set 1: "))
     A[key] = value    
-    value = float(input("Enter the Membership value for Set 2: "))
+    value = float(input(f"Enter the Membership value for {key} in Set 2: "))
     B[key] = value
 
 LHS = compliment(union(A, B))
 RHS = union(compliment(A), compliment(B))
-
-print(f"Fuzzy sets given {A=} and {B=}")
+print(f"Fuzzy sets given  {A=} and {B=}")
 if LHS == RHS:
-    print(f"DeMorgans is proved \n Since {LHS} = {RHS}")
+    print(f"DeMorgans (UNION) is proved  \n Since {LHS} = {RHS}")
+
+LHS = compliment(intersection(A, B))
+RHS = intersection(compliment(A), compliment(B))
+print(f"Fuzzy sets given  {A=} and {B=}")
+if LHS == RHS:
+    print(f"DeMorgans (INTERSECTION) is proved  \n Since {LHS} = {RHS}")
