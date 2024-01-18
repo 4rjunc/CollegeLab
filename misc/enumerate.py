0f87225b550e1578a1da1bd710e042619a0d6738
ARJUN C <arjunmundamani@gmail.com>
2024-01-18 13:10:08 +0530

    Enumerate 🚀

---
+++ misc/enumerate.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "mango", "grapes"]
+
+for i in range(len(fruits)):
+    print(f"Index {i} is {fruits[i]}")
+
+for i, fruit in enumerate(fruits):
+    print(f"Index {i} is {fruit}")
