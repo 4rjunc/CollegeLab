d2070a0807ab216a8c60925e986d6df3d06d90fd
ARJUN C <arjunmundamani@gmail.com>
2024-01-18 13:15:28 +0530

    Dict Comprehension

--- misc/comprehension.py
+++ misc/comprehension.py
@@ -1,4 +1,9 @@
+#List Comprehension
 numbers = [1, 2, 3, 4, 5]
-
 odd_number = [ num for num in numbers if num % 2 != 0]
 print(f"Odd numbers {odd_number}")
+
+# Dictionary Comprehension
+fruits = ["apple", "banana", "cherry", "date"]
+fruits_dict = {fruit : len(fruit) for fruit in fruits}
+print(fruits_dict)
