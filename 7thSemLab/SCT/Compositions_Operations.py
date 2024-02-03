d60c632815df14a8d93ad08b26d46c07998f855e
H4K3R13 <arjunmundamani@gmail.com>
2024-02-03 21:17:28 +0530

    Added q5

--- 7thSemLab/SCT/Compositions_Operations.py
+++
@@ -1,8 +0,0 @@
-# Composition Operations, Max-Min Compositions, Max-product
-# https://codecrucks.com/fuzzy-composition-max-min-and-max-product/
-
-import numpy as np
-
-A = np.array([[0.7, 0.8], [0.8, 0.3]])
-
-B = np.array([[0.8, 0.5, 0.4], [0.1, 0.6, 0.7]])
--- 7thSemLab/SCT/Q2.py
+++ 7thSemLab/SCT/Q2.py
@@ -6,13 +6,17 @@ X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
 y = np.array([[0], [1], [1], [0]], dtype=np.float32)
 
 # Define the neural network architecture
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(2, input_dim=2, activation='sigmoid'),  # Input layer with 2 nodes
-    tf.keras.layers.Dense(1, activation='sigmoid')  # Output layer with 1 node
-])
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Dense(
+            2, input_dim=2, activation="sigmoid"
+        ),  # Input layer with 2 nodes
+        tf.keras.layers.Dense(1, activation="sigmoid"),  # Output layer with 1 node
+    ]
+)
 
 # Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
 
 # Train the model on XOR data
 model.fit(X, y, epochs=10000, verbose=0)
